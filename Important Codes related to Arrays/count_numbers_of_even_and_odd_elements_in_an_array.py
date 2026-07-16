09f9a8e2fb79246fefb4d5a788e2e306ca6a0f71
satendra2703 <satendrachouhan2703@gmail.com>
2026-07-16 22:27:53 +0530

    solve 3 prepInsta question

---
+++ Important Codes related to Arrays/count_numbers_of_even_and_odd_elements_in_an_array.py
@@ -0,0 +1,17 @@
+#count numbers of even and odd elements in an array in python ?
+
+arr = [1, 7, 8, 4, 5, 16, 8]
+n = len(arr)
+countEven = 0
+countodd = 0
+for i in range(0, n):
+    if arr[i]%2==0 :
+        countEven += 1
+    else:
+        countodd += 1
+
+print("Even Elements count : " )
+print(countEven)
+
+print("Odd Elements count : ")
+print(countodd)
