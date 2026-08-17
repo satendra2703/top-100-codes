6aaf1b25682298048f7c15d4607f45e29dc125f5
satendra2703 <satendrachouhan2703@gmail.com>
2026-08-17 22:50:51 +0530

    solve 1 prepInsta question

---
+++ Pattern Printing/Pyramid_Star_Pattern.py
@@ -0,0 +1,14 @@
+#Pyramid Star Pattern in python ?
+
+n = int(input("Enter the number: "))
+
+for i in range(n):
+    # Print spaces
+    for j in range(n - i - 1):
+        print(" ", end="")
+
+    # Print stars
+    for j in range(2 * i + 1):
+        print("*", end="")
+
+    print()
